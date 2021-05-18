fb936e9b19de8ce9e2f47edb4c396bee0785d5b4
bjorne84 <edinbjorn@gmail.com>
2021-05-18 18:10:59 +0200

    fixat massa med allt möjligt, typ klart första sidan

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 const menuBtn = document.querySelector(".hamburgerBtn ");
+const closeBtn = document.querySelector(".closeBtn ");
 const menuHolder = document.querySelector(".navHolder");
+const mainForms = document.querySelector(".mainForms");
+mainForms;
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
     menuBtn.classList.add("open");
     menuOpen = true;
     menuHolder.style.display = "block";
+    mainForms.style.display = "none";
   } else {
     menuBtn.classList.remove("open");
     menuOpen = false;
   }
 });
+
+function closeFunction() {
+  menuBtn.classList.remove("open");
+  menuOpen = false;
+  menuHolder.style.display = "none";
+  mainForms.style.display = "block";
+  console.log("hej");
+}
